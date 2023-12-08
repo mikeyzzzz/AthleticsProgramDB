52ed6532ac8f3109e7dd9a30023d01736d00d224
mikeyzzzz <130306712+mikeyzzzz@users.noreply.github.com>
2023-12-07 22:05:49 -0500

    delete files

--- TheCage/HTML/radio.php
+++
@@ -1,9 +0,0 @@
-<form action="profile.php" method="POST">
-	<p>Please select your favorite genre:
-	<br />
-	<input type="radio" name="genre" value="rock" checked="checked" /> Rock
-	<input type="radio" name="genre" value="pop" /> Pop
-	<input type="radio" name="genre" value="jazz" /> Jazz
-	</p>
-	<input type="Submit" value = "Submit my choice"/>
-</form>
